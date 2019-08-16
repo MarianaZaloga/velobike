986b6bce95cd1d334c7abb12011b85db5bc52f5d
Mariana Zaloga <marjana985@gmail.com>
2019-08-16 11:43:11 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,3 @@
 
 new WOW().init();
 
-document.querySelector('.login-form input[type=submit]')
-    .addEventListener('click', login);
-
-function login(e) {
-    e.preventDefault();
-    fetch('login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({
-            name: document.querySelector('.login-form input[name=name]').value,
-            email: document.querySelector('.login-form input[name=email]').value,
-            mesagge: document.querySelector('.login-form textarea[name=message]').value
-        })
-    })
-        .then(_ => document.querySelector('.login-form').reset());
-}
